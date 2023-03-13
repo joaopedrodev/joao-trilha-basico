8b578ecbc4b6a413334e5a58474bd2633872aef7
João Pedro da Costa Vale <joaopedrodacostavale@gmail.com>
2023-03-12 21:37:33 -0300

    Aula 05 operadores ternários.

---
+++ tipos-variaveis/src/OperadoresAula05.java
@@ -0,0 +1,14 @@
+public class OperadoresAula05 {
+    //Operadores ternários
+
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 10;
+        b = 5;
+
+        String resultado = a==b ? "verdadeiro" : "falso"; //Operador ternário serve para retornar true ou false 
+        System.out.println(resultado);
+    }
+      
+}
