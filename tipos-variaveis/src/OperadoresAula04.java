1e18018a361c71a22cc3e129aa9568eda16eb762
João Pedro da Costa Vale <joaopedrodacostavale@gmail.com>
2023-03-12 21:18:02 -0300

    Aula 04 Operadores.

---
+++ tipos-variaveis/src/OperadoresAula04.java
@@ -0,0 +1,22 @@
+public class OperadoresAula04 {
+    public static void main(String[] args) {
+        int numero = 5;
+        //x repeticao
+        //numero++;
+        //numero = numero + 8;
+
+        //System.out.println( ++ numero); 
+        System.out.println(numero --);
+        System.out.println(numero);
+
+        //Podemos negar valores booleanos
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
